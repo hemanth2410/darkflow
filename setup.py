5ec58122798edda54c23377e832150cb0347d32e
hemanth sattenapalli <saihemanth.s@outlook.com>
2019-04-05 20:10:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import numpy
 import os
 import imp
 default = "darkflow/"
-VERSION = imp.load_source('version', os.path.join( default+'darkflow', 'version.py'))
+VERSION = imp.load_source('version', os.path.join( default+'darkflow/version.py'))
 VERSION = VERSION.__version__
 
 if os.name =='nt' :
